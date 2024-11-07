902800a01f9b1068ca1c127630340cb3e4bc6954
Kseniia66 <kseniyau996@yandex.ru>
2024-11-07 12:51:58 +0400

    step5

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Kseniia66/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Kseniia66/frontend-project-46/actions)
 
+[![Maintainability](https://api.codeclimate.com/v1/badges/a47e17c4d7bc9fb2f86d/maintainability)](https://codeclimate.com/github/Kseniia66/frontend-project-46/maintainability)
 
+[![Test Coverage](https://api.codeclimate.com/v1/badges/a47e17c4d7bc9fb2f86d/test_coverage)](https://codeclimate.com/github/Kseniia66/frontend-project-46/test_coverage)
 
 [![asciicast](https://asciinema.org/a/uZj0aQbq3U6sRKeGf49i7BbUm.svg)](https://asciinema.org/a/uZj0aQbq3U6sRKeGf49i7BbUm)
---
+++ __tests__/index.test.js
@@ -0,0 +1,45 @@
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'node:fs';
+import path from 'path';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const pathFileJsonOne = getFixturePath('file1.json');
+const pathFileJsonTwo = getFixturePath('file2.json');
+const pathFileYmlOne = getFixturePath('file1.yml');
+const pathFileYmlTwo = getFixturePath('file2.yml');
+
+const genDiffResult = genDiff;
+
+test.each([
+  [pathFileJsonOne, pathFileJsonTwo],
+  [pathFileYmlOne, pathFileYmlTwo],
+  [pathFileJsonOne, pathFileYmlTwo],
+  [pathFileYmlOne, pathFileJsonTwo],
+])('stylish', (a, b) => {
+  const pathFileResStylish = getFixturePath('resStylish.txt');
+  const contentFileResStylish = readFileSync(pathFileResStylish, 'utf-8');
+  expect(genDiffResult(a, b)).toBe(contentFileResStylish);
+});
+test.each([
+  [pathFileJsonOne, pathFileJsonTwo, 'plain'],
+  [pathFileYmlOne, pathFileYmlTwo, 'plain'],
+  [pathFileJsonOne, pathFileYmlTwo, 'plain'],
+  [pathFileYmlOne, pathFileJsonTwo, 'plain'],
+])('plain', (a, b, c) => {
+  const pathFileResPlain = getFixturePath('resPlain.txt');
+  const contentFileResPlain = readFileSync(pathFileResPlain, 'utf-8');
+  expect(genDiffResult(a, b, c)).toBe(contentFileResPlain);
+});
+test.each([
+  [pathFileJsonOne, pathFileJsonTwo, 'json'],
+  [pathFileYmlOne, pathFileYmlTwo, 'json'],
+  [pathFileJsonOne, pathFileYmlTwo, 'json'],
+  [pathFileYmlOne, pathFileJsonTwo, 'json'],
+])('json', (a, b, c) => {
+  const pathFileResJson = getFixturePath('resJson.json');
+  const contentFileResJson = readFileSync(pathFileResJson, 'utf-8');
+  expect(genDiffResult(a, b, c)).toBe(contentFileResJson);
+});
