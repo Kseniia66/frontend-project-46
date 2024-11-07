876b5725d7bd46549b351424e900c9f2a650edb6
Kseniia66 <kseniyau996@yandex.ru>
2024-11-07 19:10:14 +0400

    step8

--- Makefile
+++ Makefile
@@ -9,3 +9,6 @@ make lint:
 	
 test:
 	npm test
+
+test-coverage:
+	npm test -- --coverage
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -15,3 +15,17 @@ const format = (tree, style) => {
 };
 
 export default format;
+
+
+const chooseFormat = (treeDiff, format) => { // вывод результата по переданному формату
+  switch (format) {
+    case 'stylish':
+      return getFormatStylish(treeDiff);
+    case 'plain':
+      return getFormatPlain(treeDiff).slice(1);
+    case 'json':
+      return JSON.stringify(treeDiff, null, 2);
+    default:
+      throw new Error('Error in chooseFormat (stylish, plain, json)');
+  }
+};
