5ed24a5df8dfb7558aa994761d0be7ec1eeb7710
Kseniia66 <kseniyau996@yandex.ru>
2024-11-20 10:28:07 +0400

    fix5

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,4 +1,4 @@
-const getQuantityIndent = (depth, displacement = 2) => {
+const getIndent = (depth, displacement = 2) => {
   const basicQuantityIndent = 4;
   const quantityIndent = (depth * basicQuantityIndent) - displacement;
   return quantityIndent;
@@ -6,11 +6,11 @@ const getQuantityIndent = (depth, displacement = 2) => {
 const getCorrectValue = (value, indent, depth) => {
   if (value instanceof Object) {
     const entries = Object.entries(value);
-    const newKeyValue = entries.reduce((acc, keyValue) => {
-      const newAcc = `${acc}\n${indent.repeat(getQuantityIndent(depth, 0))}${keyValue[0]}: ${getCorrectValue(keyValue[1], indent, depth + 1)}`;
+    const newKeyValue = entries.map((keyValue) => {
+      const newAcc = `\n${indent.repeat(getIndent(depth, 0))}${keyValue[0]}: ${getCorrectValue(keyValue[1], indent, depth + 1)}`;
       return newAcc;
-    }, '');
-    return `{${newKeyValue}\n${indent.repeat(getQuantityIndent(depth - 1, 0))}}`;
+    }).join('');
+    return `{${newKeyValue}\n${indent.repeat(getIndent(depth - 1, 0))}}`;
   }
   return value;
 };
@@ -18,22 +18,23 @@ const getFormatStylish = (treeDiff, depth = 1) => {
   const indent = ' ';
   const formatStylish = treeDiff.flatMap((key) => {
     if (key.type === 'deleted') {
-      return `\n${indent.repeat(getQuantityIndent(depth))}- ${key.name}: ${getCorrectValue(key.value, indent, depth + 1)}`;
+      return `${indent.repeat(getIndent(depth))}- ${key.name}: ${getCorrectValue(key.value, indent, depth + 1)}`;
     }
     if (key.type === 'added') {
-      return `\n${indent.repeat(getQuantityIndent(depth))}+ ${key.name}: ${getCorrectValue(key.value, indent, depth + 1)}`;
+      return `${indent.repeat(getIndent(depth))}+ ${key.name}: ${getCorrectValue(key.value, indent, depth + 1)}`;
     }
     if (key.type === 'nested') {
-      return `\n${indent.repeat(getQuantityIndent(depth))}  ${key.name}: ${getFormatStylish(key.children, depth + 1)}`;
+      return `${indent.repeat(getIndent(depth))}  ${key.name}: ${getFormatStylish(key.children, depth + 1)}`;
     }
     if (key.type === 'unchanged') {
-      return `\n${indent.repeat(getQuantityIndent(depth))}  ${key.name}: ${getCorrectValue(key.value, indent, depth + 1)}`;
+      return `${indent.repeat(getIndent(depth))}  ${key.name}: ${getCorrectValue(key.value, indent, depth + 1)}`;
     }
 
-    const dataObjOne = `${indent.repeat(getQuantityIndent(depth))}- ${key.name}: ${getCorrectValue(key.value1, indent, depth + 1)}`;
-    const dataObjTwo = `${indent.repeat(getQuantityIndent(depth))}+ ${key.name}: ${getCorrectValue(key.value2, indent, depth + 1)}`;
-    return `\n${dataObjOne}\n${dataObjTwo}`;
-  }).join('');
-  return `{${formatStylish}\n${indent.repeat(getQuantityIndent(depth - 1, 0))}}`;
+    const dataObj = [
+      `${indent.repeat(getIndent(depth))}- ${key.name}: ${getCorrectValue(key.value1, indent, depth + 1)}`,
+      `${indent.repeat(getIndent(depth))}+ ${key.name}: ${getCorrectValue(key.value2, indent, depth + 1)}`];
+    return dataObj;
+  }).join('\n');
+  return `{\n${formatStylish}\n${indent.repeat(getIndent(depth - 1, 0))}}`;
 };
 export default getFormatStylish;
