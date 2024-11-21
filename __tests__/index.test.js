48493dd79230ad7dd3e47c46039533d7c7105913
Kseniia66 <kseniyau996@yandex.ru>
2024-11-21 20:05:36 +0400

    fix6

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,39 +7,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const pathFileJsonOne = getFixturePath('file1.json');
-const pathFileJsonTwo = getFixturePath('file2.json');
-const pathFileYmlOne = getFixturePath('file1.yml');
 const pathFileYmlTwo = getFixturePath('file2.yml');
 
 const genDiffResult = genDiff;
 
+const formatters = ['stylish', 'plain', 'json']; 
+
 test.each([
-  [pathFileJsonOne, pathFileJsonTwo],
-  [pathFileYmlOne, pathFileYmlTwo],
   [pathFileJsonOne, pathFileYmlTwo],
-  [pathFileYmlOne, pathFileJsonTwo],
-])('stylish', (a, b) => {
-  const pathFileResStylish = getFixturePath('resultStylish');
-  const contentFileResStylish = readFileSync(pathFileResStylish, 'utf-8');
-  expect(genDiffResult(a, b)).toBe(contentFileResStylish);
-});
-test.each([
-  [pathFileJsonOne, pathFileJsonTwo, 'plain'],
-  [pathFileYmlOne, pathFileYmlTwo, 'plain'],
-  [pathFileJsonOne, pathFileYmlTwo, 'plain'],
-  [pathFileYmlOne, pathFileJsonTwo, 'plain'],
-])('plain', (a, b, c) => {
-  const pathFileResPlain = getFixturePath('resultPlain');
-  const contentFileResPlain = readFileSync(pathFileResPlain, 'utf-8');
-  expect(genDiffResult(a, b, c)).toBe(contentFileResPlain);
-});
-test.each([
-  [pathFileJsonOne, pathFileJsonTwo, 'json'],
-  [pathFileYmlOne, pathFileYmlTwo, 'json'],
-  [pathFileJsonOne, pathFileYmlTwo, 'json'],
-  [pathFileYmlOne, pathFileJsonTwo, 'json'],
-])('json', (a, b, c) => {
-  const pathFileResJson = getFixturePath('resultJson');
-  const contentFileResJson = readFileSync(pathFileResJson, 'utf-8');
-  expect(genDiffResult(a, b, c)).toBe(contentFileResJson);
+])('generate difference using different format', (a, b) => {
+  formatters.forEach((formatter) => {
+    const pathFileResult = getFixturePath(`result${formatter.charAt(0).toUpperCase() + formatter.slice(1)}`);
+    const contentFileResult = readFileSync(pathFileResult, 'utf-8');
+    expect(genDiffResult(a, b, formatter)).toBe(contentFileResult);
+  });
 });
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,16 +1,16 @@
 const getIndent = (depth, displacement = 2) => {
   const basicQuantityIndent = 4;
   const quantityIndent = (depth * basicQuantityIndent) - displacement;
-  return quantityIndent;
+  return ' '.repeat(Math.max(quantityIndent, 0));
 };
 const getCorrectValue = (value, indent, depth) => {
   if (value instanceof Object) {
     const entries = Object.entries(value);
     const newKeyValue = entries.map((keyValue) => {
-      const newAcc = `\n${indent.repeat(getIndent(depth, 0))}${keyValue[0]}: ${getCorrectValue(keyValue[1], indent, depth + 1)}`;
+      const newAcc = `\n${getIndent(depth, 0)}${keyValue[0]}: ${getCorrectValue(keyValue[1], indent, depth + 1)}`;
       return newAcc;
     }).join('');
-    return `{${newKeyValue}\n${indent.repeat(getIndent(depth - 1, 0))}}`;
+    return `{${newKeyValue}\n${getIndent(depth - 1, 0)}}`;
   }
   return value;
 };
@@ -18,23 +18,23 @@ const getFormatStylish = (treeDiff, depth = 1) => {
   const indent = ' ';
   const formatStylish = treeDiff.flatMap((key) => {
     if (key.type === 'deleted') {
-      return `${indent.repeat(getIndent(depth))}- ${key.name}: ${getCorrectValue(key.value, indent, depth + 1)}`;
+      return `${getIndent(depth)}- ${key.name}: ${getCorrectValue(key.value, indent, depth + 1)}`;
     }
     if (key.type === 'added') {
-      return `${indent.repeat(getIndent(depth))}+ ${key.name}: ${getCorrectValue(key.value, indent, depth + 1)}`;
+      return `${getIndent(depth)}+ ${key.name}: ${getCorrectValue(key.value, indent, depth + 1)}`;
     }
     if (key.type === 'nested') {
-      return `${indent.repeat(getIndent(depth))}  ${key.name}: ${getFormatStylish(key.children, depth + 1)}`;
+      return `${getIndent(depth)}  ${key.name}: ${getFormatStylish(key.children, depth + 1)}`;
     }
     if (key.type === 'unchanged') {
-      return `${indent.repeat(getIndent(depth))}  ${key.name}: ${getCorrectValue(key.value, indent, depth + 1)}`;
+      return `${getIndent(depth)}  ${key.name}: ${getCorrectValue(key.value, indent, depth + 1)}`;
     }
 
     const dataObj = [
-      `${indent.repeat(getIndent(depth))}- ${key.name}: ${getCorrectValue(key.value1, indent, depth + 1)}`,
-      `${indent.repeat(getIndent(depth))}+ ${key.name}: ${getCorrectValue(key.value2, indent, depth + 1)}`];
+      `${getIndent(depth)}- ${key.name}: ${getCorrectValue(key.value1, indent, depth + 1)}`,
+      `${getIndent(depth)}+ ${key.name}: ${getCorrectValue(key.value2, indent, depth + 1)}`];
     return dataObj;
   }).join('\n');
-  return `{\n${formatStylish}\n${indent.repeat(getIndent(depth - 1, 0))}}`;
+  return `{\n${formatStylish}\n${getIndent(depth - 1, 0)}}`;
 };
 export default getFormatStylish;
